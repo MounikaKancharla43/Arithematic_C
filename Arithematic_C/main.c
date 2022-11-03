9bde98209c6978d8287c96c5db9c999f21858675
MounikaKancharla43 <117395580+MounikaKancharla43@users.noreply.github.com>
2022-11-03 22:09:37 +0100

    getting numbers

--- Arithematic_C/main.c
+++ Arithematic_C/main.c
@@ -3,6 +3,10 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int a=0,b=0,c=0;
+    printf("Enter new numbers : \n");
+    scanf("%d%d", &a ,&b);
+    printf("a is : %d \n b is :%d" , a, b);
+
     return 0;
 }

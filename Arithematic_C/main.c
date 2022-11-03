1b3834eb878e8c5441fe93904b802a60cdf5a9d9
MounikaKancharla43 <117395580+MounikaKancharla43@users.noreply.github.com>
2022-11-03 22:59:13 +0100

    remove unnecessary variable

--- Arithematic_C/main.c
+++ Arithematic_C/main.c
@@ -3,14 +3,13 @@
 #include "addition.h"
 int main()
 {
-    int a=0,b=0,c=0;
+    int a=0,b=0;
     printf("Enter new numbers : ");
     scanf("%d%d", &a ,&b);
     printf("\n a=%d \n b=%d" , a, b);
 
     //print addition of two numbers
-    c = add(a,b);
-    printf("\naddition = %d\n",c);
+    printf("\naddition = %d\n",add(a,b));
 
 
     return 0;

b7cc7a10769d7c79568f2934fce13da6e7fe5fe9
MounikaKancharla43 <117395580+MounikaKancharla43@users.noreply.github.com>
2022-11-03 22:15:33 +0100

    written printf in simple format

--- Arithematic_C/main.c
+++ Arithematic_C/main.c
@@ -4,9 +4,9 @@
 int main()
 {
     int a=0,b=0,c=0;
-    printf("Enter new numbers : \n");
+    printf("Enter new numbers : ");
     scanf("%d%d", &a ,&b);
-    printf("a is : %d \n b is :%d" , a, b);
+    printf("\n a=%d \n b=%d" , a, b);
 
     return 0;
 }
